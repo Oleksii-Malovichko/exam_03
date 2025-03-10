33b4a6e58430135ff2940db1fb83bb3e2328be7e
alex <alex@dev-kate-log>
2025-03-10 15:26:20 +0100

    done gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,139 +1,184 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
+#include "get_next_line.h"
 
-# ifndef BUFFER_SIZE
-#	define BUFFER_SIZE 5
-# endif
-
-int	ft_strlen(char *str)
+int find_new_line(char buffer[])
 {
-	int	i = 0;
+	int i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == '\n')
+			return (i + 1);
+		i++;
+	}
+	return (-1);
+}
 
+int ft_strlen(char *str)
+{
+	int i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-int	find_new_line(char *str)
+char *copy_str(char buffer[], int till)
 {
 	int i = 0;
-	while (str[i])
+	if (!buffer || till < 0)
+		return (NULL);
+	char *result = malloc(till + 1);
+	if (!result)
+		return (NULL);
+	while (i < till)
 	{
-		if (str[i] == '\n')
-			return (i + 1);
+		result[i] = buffer[i];
 		i++;
 	}
-	return (0);
+	result[i] = '\0';
+	return (result);
 }
 
-void	deal_remainder(int start, char buffer[], char **remainder)
+char *realloc_result(char **old_result, char buffer[], int end)
 {
-	int i = 0;
-
-	*remainder = malloc(ft_strlen(buffer) - start + 1);
-	if (!*remainder)
-		return ;
-	while (start < ft_strlen(buffer))
-	{
-		(*remainder)[i] = buffer[start];
-		i++;
-		start++;
-	}
-	(*remainder)[i] = '\0';
-}
-
-int	copy_str(char **result, char buffer[], char **remainder)
-{
-	int end = find_new_line(buffer);
-
-	*result = malloc(end + 1);
-	if (!*result)
-		return (-1);
-	int i = 0;
-	while (i < end)
-	{
-		(*result)[i] = buffer[i];
-		i++;
-	}
-	(*result)[i] = '\0';
-	deal_remainder(end, buffer, remainder);
-	return (1);
-}
-
-int	overwrite_buffer(char **result, char **remainder, char buffer[])
-{
-	if (!*result)
-	{
-		return (copy_str(result, buffer, remainder));
-	}
-	char *new_result = NULL;
+	char *new_result;
 	int i = 0;
 	int j = 0;
 
-	new_result = malloc(ft_strlen(*result) + find_new_line(buffer) + 1);
+	if (!old_result || !buffer || end < 0)
+		return (NULL);
+	new_result = malloc(ft_strlen(*old_result) + end + 1);
 	if (!new_result)
-		return (-1);
-	while (i < ft_strlen(*result))
+		return (NULL);
+	while ((*old_result)[i])
 	{
-		new_result[i] = (*result)[i];
+		new_result[i] = (*old_result)[i];
 		i++;
 	}
-	while (j < find_new_line(buffer))
+	while (j < end)
 	{
 		new_result[i] = buffer[j];
 		i++;
 		j++;
 	}
 	new_result[i] = '\0';
-	free(*result);
-	*result = new_result;
-	deal_remainder(find_new_line(buffer), buffer, remainder);
+	free(*old_result);
+	return (new_result);
 }
 
-int	get_data(char **result, char **remainder, char buffer[])
+char *gen_str(char **result, char buffer[], int till)
 {
-	if (find_new_line(buffer))
-		return (overwrite_buffer(result, remainder, buffer));
-	
+	if (!*result)
+	{
+		*result = copy_str(buffer, till);
+		if (!*result)
+			return NULL;
+	}
+	else
+	{
+		*result = realloc_result(result, buffer, till);
+		if (!*result)
+			return NULL;
+	}
+}
+
+void	deal_with_remainder(char **result, char buffer[], char **remainder, int new_line)
+{
+	int i = 0;
+
+	*result = gen_str(result, buffer, new_line);
+	if (!*result)
+		return ;
+	*remainder = malloc(ft_strlen(buffer) - new_line + 1);
+	if (!*remainder)
+		return ;
+	while (buffer[new_line])
+	{
+		(*remainder)[i] = buffer[new_line];
+		i++;
+		new_line++;
+	}
+	(*remainder)[i] = '\0';
+}
+
+int get_remainder(char **result, char **remainder)
+{
+	if (!*remainder)
+		return (0);
+	int new_line = find_new_line(*remainder);
+	int i;
+	if (new_line == -1) // не найдена \n
+	{
+		*result = copy_str(*remainder, ft_strlen(*remainder));
+		if (!*result)
+			return -1;
+		free(*remainder);
+		*remainder = NULL;
+		return (0);
+	}
+	*result = copy_str(*remainder, new_line);
+	if (!*result)
+		return -1;
+	char *new_remainder = malloc(ft_strlen(*remainder) - new_line + 1);
+	if (!new_remainder)
+		return -1;
+	i = 0;
+	while ((*remainder)[new_line])
+	{
+		new_remainder[i] = (*remainder)[new_line];
+		i++;
+		new_line++;
+	}
+	new_remainder[i] = '\0';
+	free(*remainder);
+	*remainder = new_remainder;
+	return (1);
 }
 
 char *get_next_line(int fd)
 {
+	if (fd < 0)
+		return (NULL);
 	ssize_t bytes_read;
+	char buffer[BUFFER_SIZE + 1];
 	static char *remainder = NULL;
 	char *result = NULL;
-	char buffer[BUFFER_SIZE];
-	int status;
-	printf("remainder: %s\n", remainder);
+
+	int status = get_remainder(&result, &remainder);
+	if (status)
+		return result;
+	if (status == -1)
+		return (NULL);
 	while (1)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
-			return (NULL);
+			return (free(result), free(remainder), NULL);
+		buffer[bytes_read] = '\0';
 		if (bytes_read == 0)
-			return (free(remainder), result);
-		status = get_data(&result, &remainder, buffer);
-		if (status == -1)
-			return (NULL);
-		if (status)
-			return (result);
+		{
+			if (remainder)
+			{
+				free(remainder);
+				remainder = NULL;
+			}
+			if (result)
+				return result;
+			return NULL;
+		}
+		if (find_new_line(buffer) != -1)
+			return (deal_with_remainder(&result, buffer, &remainder, find_new_line(buffer)), result);
+		result = gen_str(&result, buffer, ft_strlen(buffer));
 	}
 }
 
 int main()
 {
-	int fd = open("/home/alex/42_learning/exam_03/get_next_line/file.txt", O_RDONLY);
-	char *str = get_next_line(fd);
-	printf("%s", str);
+	int fd = open("file.txt", O_RDONLY);
+	char *str;
+
 	str = get_next_line(fd);
-	// printf("%s", str);
-	// while (str)
-	// {
-	// 	printf("%s", str);
-	// 	str = get_next_line(fd);
-	// }
-	return (0);
+	while (str)
+	{
+		printf("%s", str);
+		str = get_next_line(fd);
+	}
 }
